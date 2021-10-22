27be5e35b434ca8c43db47151934116e2dcb6f4f
Gabriel <suporteacbrazil@gmail.com>
2021-10-22 11:10:07 -0300

    Finish

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,22 @@
-function nomeDaFuncao() { 
-    // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
+function tabelaMultiplicacao10() { 
+  
+    let table10 = [];
+    for (let count10 = 0; count10 <= 10; count10++) {
+        table10[count10] = [];
+        for(let secondCount10 = 0; secondCount10 <= 10; secondCount10++){
+            table10[count10].push(secondCount10);
+        }
     }
-    console.table(x)
+    console.table(table10)
+}
+
+function tabelaMultiplicacao(n) { 
+    let table = [];
+    for (let count = 0; count <= n; count++) {
+        table[count] = [];
+        for(let secondCount = 0; secondCount <= n; secondCount++){
+            table[count].push(count * secondCount);
+        }
+    }
+    console.table(table)
 }
